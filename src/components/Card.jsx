d7b9d83bf815727f85edb307d30786b1434675eb
Isadorable <isadora@blockful.io>
2026-01-15 08:42:35 -0300

    added json file to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-function Card({ title, description, tags, image }) {
+function Card({ project }) {
   return (
     <>
       <div className="flex gap-6 items-center border border-zinc-800 flex p-4 hover:bg-zinc-900 transition-all duration-300">
-      <img src={image} alt="Project Image" />
+      <img src={project.imageUrl} alt="Project Image" />
         <div className="flex flex-col justify-between h-full">
           <div>
-            <p>{title}</p>
-            <p className='text-zinc-400'>{tags}</p>
+            <p>{project.name}</p>
+            <p className='text-zinc-400'>{project.tags}</p>
           </div>
           <div>
             <p className='text-zinc-400'>
-              {description}
+              {project.description}
             </p>
           </div>
         </div>
