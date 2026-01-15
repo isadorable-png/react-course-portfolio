d9df1f02b1d79c9dfa6123fc9eaadcef71985f67
Isadorable <isadora@blockful.io>
2026-01-15 09:49:01 -0300

    feat/add pixel art icons

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Camera } from "@nsmr/pixelart-react";
 
 function Card({ project }) {
   return (
     <>
+      <div>
+      </div>
       <div className="flex gap-6 items-center border border-zinc-800 flex p-4 hover:bg-zinc-900 transition-all duration-300">
       <img src={project.imageUrl} alt="Project Image" />
         <div className="flex flex-col justify-between h-full">
@@ -11,6 +14,7 @@ function Card({ project }) {
             <p className='text-zinc-400'>{project.tags}</p>
           </div>
           <div>
+          <Camera className="w-6 h-6 text-zinc-400" />
             <p className='text-zinc-400'>
               {project.description}
             </p>
