9a828d5100abcf11fb492a9e575fc2a48f13a535
Isadorable <isadora@blockful.io>
2026-01-16 09:26:35 -0300

    fix images and card hover

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
-import { Camera } from "@nsmr/pixelart-react";
 
 function Card({ project }) {
   return (
     <>
       <div>
       </div>
-      <div className="flex gap-6 items-center border border-zinc-800 flex p-4 hover:bg-zinc-900 transition-all duration-300">
-      <img src={project.imageUrl} alt="Project Image" />
+      <div className="group flex gap-6 items-center border border-zinc-800 p-4 hover:bg-zinc-900 transition-all duration-500">
+        <img src={project.imageUrl} alt="Project Image" />
         <div className="flex flex-col justify-between h-full">
           <div>
             <p>{project.name}</p>
             <p className='text-zinc-400'>{project.tags}</p>
           </div>
           <div>
-          <Camera className="w-6 h-6 text-zinc-400" />
-            <p className='text-zinc-400'>
+            <p className="text-zinc-400 opacity-0 max-h-0 overflow-hidden group-hover:opacity-100 group-hover:max-h-20 transition-all duration-500 ease-in-out">
               {project.description}
             </p>
           </div>
